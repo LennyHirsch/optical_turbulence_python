3bf3d246c3d411c5555403a6b735e7c60a5bb650
LennyHirsch <lenny.hirsch@gmail.com>
2023-09-27 13:50:25 +0100

    Fixed Jupyter notebook. Works well with res = 512

--- lenny_propagation.py
+++ lenny_propagation.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-res = 2048 #i wouldn't recommend going too much higher than this for resolution
+res = 512 #i wouldn't recommend going too much higher than this for resolution
 screen_width = 0.01 # the cross sectional width and height of the simulation
 number_of_particles = 5000
 wvl = 6.5e-9
@@ -20,7 +20,7 @@ initial_z = 0
 beam.laguerre_gaussian_beam(mode, 0, beam_waist, initial_z)
 
 #propagate the beam some distance 
-dis = 0.3
+dis = 0.5
 beam.free_space_prop(dis)
 
 #THIS WILL NEED TO BE CHANGED TO ABSORBERS RATHER THAN A RI SCREEN BUT THE PRINCIPLE SHOULD BE THE SAME
@@ -32,21 +32,21 @@ circles = absorbers.plot_circles(grid, particle_size, res)
 beam.apply_absorber_screen(circles)
 
 #generate the phase screen - don't worry too much
-# l0 = 0.001
-# L0 = 100.0
-# r0 = 0.1
+l0 = 0.001
+L0 = 100.0
+r0 = 0.01
 
-# phz = prop.PhaseScreen(screen_width, res, r0, l0, L0)
-# phz.mvk_screen()
-# phz.mvk_sh_screen()
+phz = prop.PhaseScreen(screen_width, res, r0, l0, L0)
+phz.mvk_screen()
+phz.mvk_sh_screen()
 
 # # #APPLY THE PHASE SCREEN TO BEAM 
-# beam.apply_phase_screen(phz.phz + phz.phz_lo)
+beam.apply_phase_screen(phz.phz + phz.phz_lo)
 beam.free_space_prop(dis)
 
 #THE BEAM FIELD IS ACCESSED FROM THE VARIABLE BEAM.FIELD
 #I've plot the phase here for a quick example
 fig, (ax0, ax1) = plt.subplots(1, 2)
-ax0.imshow(np.abs(beam.field))
+ax0.imshow(np.abs(beam.field), cmap = 'plasma')
 ax1.imshow(np.angle(beam.field), cmap = 'hsv')
 plt.show()
